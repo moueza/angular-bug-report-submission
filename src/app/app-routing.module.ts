be7a8b669b2c71cc167454cb3848401b5c5ba4ae
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2023-10-23 07:11:36 +0200

    sov

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {  ProfilComponent } from './profil/profil.component';//https://angular.io/tutorial/first-app/first-app-lesson-10#step-3---add-route-to-new-component
-import { ProfileProfilEditorComponent } from './profile-profil-editor/profile-profil-editor.component';
 
 const routes: Routes = [
-			{
-			path: 'profil',
-			    component:ProfilComponent,
-			    title: 'Home profil'
-			    },
-
-	{
-			path: 'profile',
-			    component:ProfileProfilEditorComponent,
-			    title: 'Home profil'
-			    },
+		
 			];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfilComponent } from './profil/profil.component';
-
 import { ReactiveFormsModule } from '@angular/forms';
-import { PseudoEditorComponent } from './pseudo-editor/pseudo-editor.component';
-import { PseudoTiktokEditorComponent } from './pseudo-tiktok-editor/pseudo-tiktok-editor.component';
-import { ProfileProfilEditorComponent } from './profile-profil-editor/profile-profil-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfilComponent,
-    PseudoEditorComponent,
-    PseudoTiktokEditorComponent,
-    ProfileProfilEditorComponent
+ 
   ],
   imports: [
     BrowserModule,
